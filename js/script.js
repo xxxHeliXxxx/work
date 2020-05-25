85e34ae02c38c5de0b051715a837bd9356e17b6f
Oleksii <atilinin92work@gmail.com>
2020-05-25 19:06:22 +0200

    add my first app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оценете его?", "");
+const d = prompt("На сколько оценете его?", "");
+
+
+personalMovieDB.movies[a] = b;
+
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
